3e2be7c823105954f2a29482f569cf89e31c1328
Katsuya <pokopenkatuya@gmail.com>
2016-06-03 14:29:36 +0900

    登録フォームは作成したが、ラベル部分、フォームの入力したフォントカラーが真っ白になってしまう

--- app/controllers/front/top_controller.rb
+++ app/controllers/front/top_controller.rb
@@ -1,4 +1,12 @@
 class Front::TopController < FrontController
   def index
+    @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save
+    redirect_to front_top_index
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Kintore::Application.routes.draw do
   root 'front/top#index'
   scope module: :front do
     resources :menus, only: :index
+    resources :users
   end
 end
