44b4c86ad125f9a9c74c683fd73ac8f745c5939a
藤原 智弥 <al14083@shibaura-it.ac.jp>
2017-01-31 17:01:28 +0900

    直し

--- db/migrate/20160531051314_create_menus.rb
+++ db/migrate/20160531051314_create_menus.rb
@@ -2,7 +2,7 @@ class CreateMenus < ActiveRecord::Migration
   def change
     create_table :menus do |t|
       t.string  :name
-      t.text    :descriptiono
+      t.text    :description
       t.integer :condition_weight_from
       t.integer :condition_weight_to
       t.integer :condition_height_from
